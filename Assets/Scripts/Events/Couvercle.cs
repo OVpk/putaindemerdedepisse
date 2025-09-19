792da02aba9b15ca2a3e1d17e3f6f0e9b4297a15
VERFAILLIE <olivier.verfaillie59@laposte.net>
2025-09-19 06:36:25 +0200

    srgxfvyjhilo

--- Assets/Scripts/Events/Couvercle.cs
+++ Assets/Scripts/Events/Couvercle.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Couvercle : GameEvent
 {
+    public Animator animator;
+    
     public override void StartEvent()
     {
         gameObject.SetActive(true);
+        animator.SetTrigger("down");
     }
 
     public override void StopEvent()
     {
         gameObject.SetActive(false);
+        animator.SetTrigger("up");
     }
 }
